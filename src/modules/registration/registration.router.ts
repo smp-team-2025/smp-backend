50f1fc0f4ac2701b3b95450fa75ed83813690c8d
Arda <146741024+ardakrg@users.noreply.github.com>
2025-12-21 23:07:31 +0300

    bug fix

--- src/modules/registration/registration.router.ts
+++ src/modules/registration/registration.router.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { registrationController } from "./registration.controller";
 import { requireAuth, requireRole } from "../../middleware/auth";
+import { UserRole } from "@prisma/client";
 
 
 export const registrationRouter = Router();
 
 
 // Organizer-Only
-registrationRouter.get("/", requireAuth, requireRole("organizer"), registrationController.getAll);
-registrationRouter.get("/:id", requireAuth, requireRole("organizer"), registrationController.getById);
-registrationRouter.post("/:id/approve", requireAuth, requireRole("organizer"), registrationController.approve);
-registrationRouter.post("/:id/reject", requireAuth, requireRole("organizer"), registrationController.reject);
+registrationRouter.get("/", requireAuth, requireRole(UserRole.ORGANIZER), registrationController.getAll);
+registrationRouter.get("/:id", requireAuth, requireRole(UserRole.ORGANIZER), registrationController.getById);
+registrationRouter.post("/:id/approve", requireAuth, requireRole(UserRole.ORGANIZER), registrationController.approve);
+registrationRouter.post("/:id/reject", requireAuth, requireRole(UserRole.ORGANIZER), registrationController.reject);
 
 // Public Registrations (students)
 registrationRouter.post("/", registrationController.create);
